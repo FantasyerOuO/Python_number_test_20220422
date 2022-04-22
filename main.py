cf2390105ca2b90a66165720b05a032ed610b10a
FantasyerOuO <75434717+FantasyerOuO@users.noreply.github.com>
2022-04-22 23:40:26 +0800

    add resizeable
    使用resizeable鎖住視窗大小更動

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from cProfile import label
+
+
 import tkinter as tk
 from turtle import color
 # 建立視窗Create a window
 window_0 = tk.Tk()
 # 設定視窗大小
 window_0.geometry('400x600')
+#設定視窗可調整
+window_0.resizable(False,False)
+
 # 設定視窗標題
 window_0.title('數字計算視窗編寫')
 
